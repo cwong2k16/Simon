bca31007865e75f9e2ed03ba510f69cef3073946
cwong2015 <sirhc8002@gmail.com>
2017-08-08 14:31:57 -0400

    button colors changes when player clicks, then goes back to original color after .25 seconds

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+var btnIDs = ["greenBtn", "redBtn", "yellowBtn", "blueBtn"];
+var lightIDs = {"greenBtn": "#4caf50", "yellowBtn": "#ffeb3b", "redBtn": "#f44336", "blueBtn": "#2196f3"};
+var darkIDs = {"greenBtn": "#388e3c", "yellowBtn": "#fbc02d", "redBtn": "#d32f2f", "blueBtn": "#1976d2"};
+
 $(document).ready(function(){
-  
+  $("button").click(function(){
+    if(btnIDs.includes(this.id)){
+        $("#" + this.id).css("background", lightIDs[this.id]);
+        var id = this.id;
+        setTimeout(function(){
+            console.log(id + " " + darkIDs[id]);
+            $("#" + id).css("background", darkIDs[id]);
+        }, 500);
+    }
+  });
 });
