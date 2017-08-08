c0bc6fcfb710144a49029a9a9ad016dc1b9e6728
cwong2015 <sirhc8002@gmail.com>
2017-08-08 14:39:49 -0400

    made single object for brightness changes instead of having two objects for dark and light

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 var btnIDs = ["greenBtn", "redBtn", "yellowBtn", "blueBtn"];
-var lightIDs = {"greenBtn": "#4caf50", "yellowBtn": "#ffeb3b", "redBtn": "#f44336", "blueBtn": "#2196f3"};
-var darkIDs = {"greenBtn": "#388e3c", "yellowBtn": "#fbc02d", "redBtn": "#d32f2f", "blueBtn": "#1976d2"};
-
+var brightness = {"greenBtn": {"light": "#4caf50", "dark": "#388e3c"}, "yellowBtn": {"light": "#ffeb3b", "dark":"#fbc02d"}, "redBtn": {"light": "#f44336", "dark": "#d32f2f"}, "blueBtn": {"light": "#2196f3", "dark": "#1976d2"}};
 $(document).ready(function(){
   $("button").click(function(){
     if(btnIDs.includes(this.id)){
-        $("#" + this.id).css("background", lightIDs[this.id]);
+        $("#" + this.id).css("background", brightness[this.id]["light"]);
         var id = this.id;
         setTimeout(function(){
-            console.log(id + " " + darkIDs[id]);
-            $("#" + id).css("background", darkIDs[id]);
+            $("#" + id).css("background", brightness[id]["dark"]);
         }, 500);
     }
   });
