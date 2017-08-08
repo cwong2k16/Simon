39ea341152125e5f056129bc2655da3b5c69ddfd
cwong2015 <sirhc8002@gmail.com>
2017-08-08 15:23:02 -0400

    changes brightness to btnProps and added audios to the objects, buttons play sounds when clicked

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
+var gbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+var rbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+var ybs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+var bbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 var btnIDs = ["greenBtn", "redBtn", "yellowBtn", "blueBtn"];
-var brightness = {"greenBtn": {"light": "#4caf50", "dark": "#388e3c"}, "yellowBtn": {"light": "#ffeb3b", "dark":"#fbc02d"}, "redBtn": {"light": "#f44336", "dark": "#d32f2f"}, "blueBtn": {"light": "#2196f3", "dark": "#1976d2"}};
+var btnProps = {"greenBtn": {"light": "#4caf50", "dark": "#388e3c", "audio": gbs}, "yellowBtn": {"light": "#ffeb3b", "dark":"#fbc02d", "audio": ybs}, "redBtn": {"light": "#f44336", "dark": "#d32f2f", "audio": rbs}, "blueBtn": {"light": "#2196f3", "dark": "#1976d2", "audio": bbs}};
 $(document).ready(function(){
   $("button").click(function(){
     if(btnIDs.includes(this.id)){
-        $("#" + this.id).css("background", brightness[this.id]["light"]);
+        console.log(this.id);
+        $("#" + this.id).css("background", btnProps[this.id]["light"]);
+        btnProps[this.id]["audio"].play();
         var id = this.id;
         setTimeout(function(){
-            $("#" + id).css("background", brightness[id]["dark"]);
+            $("#" + id).css("background", btnProps[id]["dark"]);
         }, 500);
     }
   });
