dbd6c4dbeb0e00295d2e037053dcf87c450e0c9d
cwong2015 <sirhc8002@gmail.com>
2017-08-08 15:57:30 -0400

    added tester to play button lights for user to emulate

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var ybs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 var bbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
 var btnIDs = ["greenBtn", "redBtn", "yellowBtn", "blueBtn"];
 var btnProps = {"greenBtn": {"light": "#4caf50", "dark": "#388e3c", "audio": gbs}, "yellowBtn": {"light": "#ffeb3b", "dark":"#fbc02d", "audio": ybs}, "redBtn": {"light": "#f44336", "dark": "#d32f2f", "audio": rbs}, "blueBtn": {"light": "#2196f3", "dark": "#1976d2", "audio": bbs}};
+var colorsToShow = ["redBtn", "blueBtn", "greenBtn"];
 $(document).ready(function(){
   $("button").click(function(){
     if(btnIDs.includes(this.id)){
@@ -14,6 +15,21 @@ $(document).ready(function(){
         setTimeout(function(){
             $("#" + id).css("background", btnProps[id]["dark"]);
         }, 500);
+    }
+    else if(this.id === "play"){
+        var index = 0;
+        setInterval(function(){
+        if(index < colorsToShow.length){
+            $("#" + colorsToShow[index]).css("background", btnProps[colorsToShow[index]]["light"]);
+            var id = colorsToShow[index];
+            var thisBtn = btnProps[id];
+            console.log(thisBtn);
+            setTimeout(function(){            
+                $("#" + id).css("background", btnProps[id]["dark"]);
+            }, 500);
+        index++;    
+        }   
+       }, 1000);
     }
   });
 });
