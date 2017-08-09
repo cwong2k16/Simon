6b14ec73134e5bbbd1a7b3d7d5b26e616ca23735
cwong2015 <sirhc8002@gmail.com>
2017-08-08 23:50:19 -0400

    added correct/incorrect indicator, and steps

--- js/index.js
+++ js/index.js
@@ -2,9 +2,12 @@ var gbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
 var rbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
 var ybs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
 var bbs = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+var index = 0;
+var score = 1;
 var btnIDs = ["greenBtn", "redBtn", "yellowBtn", "blueBtn"];
 var btnProps = {"greenBtn": {"light": "#4caf50", "dark": "#388e3c", "audio": gbs}, "yellowBtn": {"light": "#ffeb3b", "dark":"#fbc02d", "audio": ybs}, "redBtn": {"light": "#f44336", "dark": "#d32f2f", "audio": rbs}, "blueBtn": {"light": "#2196f3", "dark": "#1976d2", "audio": bbs}};
-var colorsToShow = ["redBtn"];
+var randIndex = Math.floor(Math.random()*4);
+var colorsToShow = [btnIDs[randIndex]];
 var playerEmulate = [];
 $(document).ready(function(){
   $("button").click(function(){
@@ -15,14 +18,19 @@ $(document).ready(function(){
         var a = check();
         if(a !== "aaa"){
             if(a){
-                alert("You won!");
+                index = 0;
+                $("#correct").html("correct!!");
+                score++;
+                $("#steps").html(score);
+                console.log("Correct! Current score: " + score + ".");
                 var rand = Math.floor(Math.random() * 4);
                 colorsToShow.push(btnIDs[rand]);
-                console.log(colorsToShow);
                 playerEmulate = [];
             }
             else{
-                alert("You lose!");
+                index = 0;
+                $("#correct").html("incorrect!!");
+                console.log("You lose!");
                 playerEmulate = [];
             }
         }
@@ -32,7 +40,6 @@ $(document).ready(function(){
         }, 500);
     }
     else if(this.id === "play"){
-        var index = 0;
         setInterval(function(){
         if(index < colorsToShow.length){
             $("#" + colorsToShow[index]).css("background", btnProps[colorsToShow[index]]["light"]);
